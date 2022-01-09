f183844eefc98f6c00b27d7a95e059981668cc67
Yuvam <67573209+yuvam2017@users.noreply.github.com>
2022-01-09 20:21:40 +0530

    Add files via upload

---
+++ wifi-add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install git build-essential dkms
+sudo git clone https://github.com/tomaspinho/rtl8821ce
+cd rtl8821ce
+chmod +x dkms-install.sh
+chmod +x dkms-remove.sh
+sudo ./dkms-install.sh
